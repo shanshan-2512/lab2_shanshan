2aff0f35460d733e65ab5c1c5c3f748f4df17cfb
HeritageCollege <1896768@cegep-heritage.qc.ca>
2022-03-10 14:37:19 -0500

    add users

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  res.send('you find me!');
+});
+
+router.get('/ineedrest', function (req, res, next) {
+  res.send('Please leave me alone!');
 });
 
 module.exports = router;
